7289e674bbec5f193e40aa32936663a49fa77b6c
kbudkiewicz <krystianbudkiewicz@gmail.com>
2025-05-10 20:06:57 +0200

    chore: update FFN

--- networks.py
+++ networks.py
@@ -26,21 +26,30 @@ class LinearBlock(nn.Module):
 
 
 class FeedForwardNetwork(nn.Module):
-    def __init__(self, *dims,
-                 activation: nn.Module = nn.ReLU(),
-                 regularizer: nn.Module = nn.Dropout(p=0.1)):
+    def __init__(self,
+                 *dims,
+                 activation: nn.Module = nn.ReLU,
+                 normalization: nn.Module = nn.Dropout(p=0.1),
+                 device=None):
         super().__init__()
-        self.dims = dims
-        self.module_list = nn.ModuleList()
-        self.activation = activation
-        self.regularizer = regularizer
+        if device:
+            self.device = device
+        else:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-        for idx in range(len(self.dims) - 2):
-            self.module_list.append(nn.Linear(self.dims[idx], self.dims[idx + 1]))
-            self.module_list.append(self.activation)
-            self.module_list.append(self.regularizer)
-        self.module_list.append(nn.Linear(dims[-2], dims[-1]))  # last layer without activation
-        self.module_list.append(self.regularizer)
+        self.net = nn.Sequential()
+        for idx in range(len(dims) - 1):
+            self.net.append(
+                LinearBlock(
+                    dims[idx], dims[idx + 1],
+                    activation=activation,
+                    normalization=normalization,
+                    device=self.device,
+                )
+            )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.net(x)
 
         self.net = nn.Sequential(*self.module_list)
 
