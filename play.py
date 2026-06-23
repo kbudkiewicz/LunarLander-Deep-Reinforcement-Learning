88664452d9d3d9947c7eacefdde41f2213b9eddf
kbudkiewicz <krystianbudkiewicz@gmail.com>
2026-06-23 18:52:31 +0200

    chore: add play.py
    Add a script that automatically checks for the best model saved in the
    registry, loads its weights into the agent and lets it play in the human
    rendered environment.

---
+++ play.py
@@ -0,0 +1,62 @@
+import argparse
+import torch
+import mlflow
+import gymnasium as gym
+
+from agent import Agent
+
+
+if __name__ == '__main__':
+    argsparer = argparse.ArgumentParser(
+        description='Visualize the best agent for a given environment saved in the model registry.'
+    )
+    argsparer.add_argument('-E', '--environment-id', type=str, required=False, default='LunarLander-v3',
+        help='Identifier of the environment'
+    )
+    argsparer.add_argument('-e', '--epochs', type=int, default=10, help='Number of epoch to play')
+    argsparer.add_argument('-s', '--max-episode-steps', type=int, default=500,
+        help='Maximal number of allowed steps in the environment per epoch'
+    )
+    argsparer.add_argument('-D', '--device', type=str)
+    argsparer.add_argument('-m', '--model-name', type=str, default='qnet_local')
+    args = argsparer.parse_args()
+
+    if args.device is not None:
+        device = args.device
+    else:
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    try:
+        env = gym.make(id=args.environment_id, max_episode_steps=args.max_episode_steps, render_mode='human')
+    except ValueError as e:
+        raise e
+
+    # get latest model version
+    client = mlflow.tracking.MlflowClient()
+    latest_version = client.get_latest_versions(name=args.model_name, stages=['None', 'Staging', 'Production'])
+    if len(latest_version) == 0:
+        raise mlflow.exceptions.MlflowException('No model was found for the specified environment')
+
+    # import the latest model
+    latest = max(latest_version, key=lambda x: int(x.version))
+    model_uri = f"models:/qnet_local/{latest.version}"
+    qnet_local = mlflow.pytorch.load_model(model_uri=model_uri, device=device)
+    model_uri = f"models:/qnet_target/{latest.version}"
+    qnet_target = mlflow.pytorch.load_model(model_uri=model_uri, device=device)
+
+    agent = Agent(
+        qnet_local=qnet_local, qnet_target=qnet_target, criterion=None, device=device, action_space=env.action_space.n,
+        inference_only=True
+    )
+    agent.zero_epsilon()
+
+    for _ in range(args.epochs):
+        terminated = truncated = False
+        state, _ = env.reset()
+        while not (truncated or terminated):
+            action = agent(state)
+            new_state, _, terminated, truncated, _ = env.step(action)
+            state = new_state
+
+    env.close()
+    exit(0)
