c699871ed242a6e4e2645aad2262a7b87eb3b94f
kbudkiewicz <krystianbudkiewicz@gmail.com>
2026-07-04 16:10:39 +0200

    chore: add tests for DQN and DoubleDQN

---
+++ tests/test_agents.py
@@ -0,0 +1,138 @@
+import pytest
+import torch
+import gymnasium as gym
+import numpy as np
+import copy
+
+from typing import Tuple
+from torch.nn import Module
+from nn import FeedForwardNetwork, DuelingQNetwork
+from agent import Agent, DeepQNetwork, DoubleDQN, DuelingDQN
+
+
+class BaseAgentTest:
+    @pytest.fixture(params=[0, 1, 42, 12345])
+    def seed(self, request) -> int:
+        """Set seed for test reproducibility."""
+        return request.param
+
+    @pytest.fixture(autouse=True)
+    def _set_seed(self, seed):
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+    @pytest.fixture(scope='module')
+    def device(self) -> torch.device:
+        return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    @pytest.fixture
+    def env_id(self) -> str:
+        return 'LunarLander-v3'
+
+    @pytest.fixture
+    def max_episode_steps(self) -> int:
+        return 20
+
+    @pytest.fixture
+    def env(self, env_id, max_episode_steps) -> gym.Env:
+        """Build a ``gymnasium`` environment."""
+        environment = gym.make(id=env_id, max_episode_steps=max_episode_steps)
+        yield environment
+        environment.close()
+
+    @pytest.fixture
+    def state_space(self, env: gym.Env) -> Tuple[int, ...]:
+        return env.observation_space.shape
+
+    @pytest.fixture
+    def action_space(self, env: gym.Env) -> int:
+        return env.action_space.n
+
+    @pytest.fixture
+    def dims(self, state_space, action_space) -> tuple[int, ...]:
+        return *state_space, np.random.randint(2, 32), np.random.randint(2, 32), action_space
+
+    @pytest.fixture
+    def criterion(self) -> Module:
+        # assert criterion_name in torch.nn.modules.loss
+        return torch.nn.MSELoss()
+
+    @pytest.fixture
+    def network(self, dims, device: torch.device) -> Module:
+        """Build a neural network that will be used for initializing the Agent."""
+        raise NotImplementedError
+
+    @pytest.fixture
+    def agent(self, *args, **kwargs) -> Agent:
+        """Initialize the Agent."""
+        raise NotImplementedError
+
+    @staticmethod
+    def assert_action_is_valid(action, action_space):
+        """Assert the agent's action is within a discrete action space."""
+        if isinstance(action, np.ndarray):
+            assert np.isfinite(action).all()
+            assert action.shape[0] == action_space
+            assert np.all((0 <= action) & (action < action_space))
+        elif isinstance(action, (int, np.integer)):
+            assert 0 <= action < action_space
+        else:
+            raise ValueError(f'Unexpected action type: {type(action)}.')
+
+    @staticmethod
+    def assert_replay_memory_within_range(agent, current_step: int):
+        """Check if agent saves experiences in replay memory."""
+        expected_len = min(current_step + 1, agent.memory.memory.maxlen)
+        assert len(agent.memory) == expected_len
+
+    def test_interact(self, seed: int, agent: Agent, env: gym.Env, action_space: int) -> None:
+        state, _ = env.reset(seed=seed)
+        max_episode_steps = env.spec.max_episode_steps
+
+        for step in range(max_episode_steps):
+            action = agent(state)
+            self.assert_action_is_valid(action, action_space)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            _, _ = agent.memorize(state, action, reward, next_state, terminated)
+            self.assert_replay_memory_within_range(agent, step)
+            state = next_state
+            if terminated or truncated:
+                break
+
+
+class TestDQN(BaseAgentTest):
+    @pytest.fixture
+    def network(self, dims, device) -> FeedForwardNetwork:
+        return FeedForwardNetwork(*dims, device=device)
+
+    @pytest.fixture
+    def agent(self, network, criterion, action_space, device):
+        local = network
+        target = copy.deepcopy(local)
+        return DeepQNetwork(
+            local=local,
+            target=target,
+            criterion=criterion,
+            action_space=action_space,
+            device=device,
+        )
+
+
+class TestDoubleDQN(BaseAgentTest):
+    @pytest.fixture
+    def network(self, dims, device) -> FeedForwardNetwork:
+        return FeedForwardNetwork(*dims, device=device)
+
+    @pytest.fixture
+    def agent(self, network, criterion, action_space, device):
+        local = network
+        target = copy.deepcopy(local)
+        return DoubleDQN(
+            local=local,
+            target=target,
+            criterion=criterion,
+            action_space=action_space,
+            device=device,
+        )
+
+
---
+++ tests/test_net.py
@@ -0,0 +1,93 @@
+import pytest
+import torch
+import numpy as np
+
+from typing import Callable, Tuple
+from torch import Tensor
+from torch.nn import Module
+from nn import FeedForwardNetwork
+
+
+class BaseNetworkTest:
+    @pytest.fixture(scope="session")
+    def device(self) -> torch.device:
+        return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    @pytest.fixture(params=[1, 2, np.random.randint(3, 32)])
+    def batch_size(self, request) -> int:
+        return request.param
+
+    @pytest.fixture
+    def state_space(self) -> int:
+        return 4
+
+    @pytest.fixture
+    def action_space(self) -> int:
+        return 8
+
+    @pytest.fixture
+    def build_model(self, *args, **kwargs) -> Callable[..., Module]:
+        raise NotImplementedError
+
+    @pytest.fixture
+    def model(self, build_model, dims, device) -> Module:
+        return build_model(*dims, device=device)
+
+    @staticmethod
+    def assert_model_output(x: Tensor, batch_size: int, action_space: int, is_batched: bool = True):
+        assert isinstance(x, Tensor)
+        assert torch.isfinite(x).all()
+        if is_batched:
+            assert x.shape == (batch_size, action_space)
+        else:
+            assert x.shape == (1, action_space)
+
+    @staticmethod
+    def test_init_invalid_dims(dims_invalid, build_model, device):
+        with pytest.raises(ValueError):
+            build_model(*dims_invalid, device=device)
+
+    @staticmethod
+    def test_init_valid_dims(dims, build_model, device):
+        net = build_model(*dims, device=device)
+        assert isinstance(net, Module)
+
+    def test_forward_batched(self, model, state_space, action_space, batch_size, device):
+        x = torch.randn(batch_size, state_space).to(device=device)
+        out = model.forward(x)
+        self.assert_model_output(out, batch_size=batch_size, action_space=action_space, is_batched=True)
+
+    def test_forward_single(self, model, state_space, action_space, device):
+        x = torch.randn(1, state_space).to(device=device)
+        out = model.forward(x)
+        self.assert_model_output(out, batch_size=1, action_space=action_space, is_batched=False)
+
+    @staticmethod
+    def test_last_layer_is_identity(model, device):
+        last_module = list(model.modules())[-1]
+        assert isinstance(last_module, torch.nn.Identity)
+
+
+class TestFeedForwardNetwork(BaseNetworkTest):
+    @pytest.fixture(
+        params=[(4, 8), (4, 1, 8), (4, 128, 128, 8)],
+        ids=["no_hidden_dimension", "minimal", "normal"]
+    )
+    def dims(self, request) -> Tuple[int, ...]:
+        """Set of valid model dimensions"""
+        return request.param
+
+    @pytest.fixture(
+        params=[(0,), (1,), (0, 1, 8), (4, 1, 0)],
+        ids=["zero_dimension", "single_dimension", "zero_dimension_in", "zero_dimension_out"]
+    )
+    def dims_invalid(self, request) -> Tuple[int, ...]:
+        """Set of invalid model dimensions"""
+        return request.param
+
+    @pytest.fixture
+    def build_model(self) -> Callable[..., FeedForwardNetwork]:
+        def _model(*args, **kwargs):
+            return FeedForwardNetwork(*args, **kwargs)
+        return _model
+
